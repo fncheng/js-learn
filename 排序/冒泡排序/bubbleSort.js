bb99acef165e457cf435b50e0e22aca3efb3eba8
cheng <dong862647669@gmail.com>
2021-03-08 22:42:20 +0800

    冒泡排序

---
+++ 排序/冒泡排序/bubbleSort.js
@@ -0,0 +1,30 @@
+function bubbleSort(array, compareFn = defaultCompare) {
+  const { length } = array;
+  let count = 0;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1 - i; j++) {
+      count++;
+      if (compareFn(array[j], array[j + 1])) {
+        swap(array, j, j + 1);
+        // [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  console.log(count);
+  return array;
+}
+let defaultCompare = function (a, b) {
+  return a > b ? true : false;
+};
+/**
+ * 交换数组下标1和下标2的值
+ * @param {Array} array 交换的数组
+ * @param {Number} a 下标1
+ * @param {Number} b 下标2
+ */
+function swap(array, a, b) {
+  [array[a], array[b]] = [array[b], array[a]];
+}
+
+let res = bubbleSort([5, 4, 3, 2, 1]);
+console.log(res);
